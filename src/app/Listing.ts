a3ff5835aa82da868810276217d3ee8b5728cdbe
garzoku <garzoku@WGC10316TQCB3.zld.dc.ford.com>
2022-05-29 16:05:52 -0500

    create Listing class

---
+++ src/app/Listing.ts
@@ -0,0 +1,10 @@
+export class Listing {
+  name!: string;
+  websiteUrl?: string;
+  directionsUrl!: string;
+  description!: string;
+  hourBeginning?: string;
+  hourEnding?: string;
+  dateBeginning?: string;
+  dateEnding?: string;
+}
