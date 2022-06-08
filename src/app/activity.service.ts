9fe5c53768ecac282d5bfc88dcdd1e8e053721d7
garzoku <garzoku@WGC10316TQCB3.zld.dc.ford.com>
2022-06-07 19:32:04 -0500

    build out ngForm to add event

--- src/app/activity.service.ts
+++ src/app/activity.service.ts
@@ -2,10 +2,13 @@ import { Injectable } from '@angular/core';
 import {Activity} from "./Activity"
 
 import { HttpClient } from '@angular/common/http'
+import { environment } from 'src/environments/environment';
 
 type ActivityReponse = {
   activities: Activity[];
 }
+
+const abaseApiUrl = environment.baseApiUrl;
 
 @Injectable({
   providedIn: 'root'
@@ -15,6 +18,6 @@ export class ActivityService {
 constructor(private http: HttpClient){}
 
 get activityList(){
-  return this.http.get<ActivityReponse>("../assets/activities.json")
+  return this.http.get<ActivityReponse>(`${environment.baseApiUrl}/api/activities`)
   }
 }
--- src/app/add-whats-popn/add-whats-popn.component.ts
+++ src/app/add-whats-popn/add-whats-popn.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { ExpressionBinding } from '@angular/compiler';
+import { Component} from '@angular/core';
+
+type FormData = {
+  name:string,
+  address: string,
+  website: string,
+  description:string,
+  hourBeginning:string,
+  hourEnding:string,
+  dateBeginning:String,
+  dateEnding:string
+}
 
 @Component({
   selector: 'app-add-whats-popn',
   templateUrl: './add-whats-popn.component.html',
   styleUrls: ['./add-whats-popn.component.css']
 })
-export class AddWhatsPopnComponent implements OnInit {
+export class AddWhatsPopnComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  addEvent({name, address, website, description, hourBeginning, hourEnding, dateBeginning, dateEnding}: FormData){
+    console.log(name, address, website, description, hourBeginning, hourEnding, dateBeginning, dateEnding);
   }
 
 }
